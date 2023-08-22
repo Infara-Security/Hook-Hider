8a9d882ca5d5e0f62d49051ce8bb3d45f321d71a
wang <114349746+wang-on-top@users.noreply.github.com>
2023-08-22 19:13:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,33 @@
 window.hook = { secret : "yes" };
 window.key = hideHook(hook)
 
-window.getSecret = () => {
-  return document.getElementById(key).hook.secret
+window.getHook = () => {
+  return document.getElementById(key)[' '](key)
 }
 */
 function hideHook (hook) {
+    function generateSecureKey(length) {
+      var result           = '';
+      var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+      var charactersLength = characters.length;
+      for ( var i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
     let obContainer = document.createElement('span');
     let obElement = document.createElement('span');
-    let obKey = Math.random().toString(36).substring(2, 15); // Generate a more complex key
+    let obKey = generateSecureKey(32)
     obElement.id = obKey;
     obContainer.style.display = 'none'; // Hide the container
     obContainer.appendChild(obElement);
     document.body.appendChild(obContainer);
-    document.getElementById(obKey).hook = hook;
+    document.getElementById(obKey)[' '] = (pass) => {
+        if(pass == obKey) {
+          return hook
+        } else {
+          return {}
+        }
+    };
     return obKey;
 }
